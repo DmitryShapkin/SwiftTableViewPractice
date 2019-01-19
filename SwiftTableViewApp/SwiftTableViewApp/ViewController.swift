f41fb028170c805b77d04f4270aa4fa6a0d9a623
Dmitry Shapkin <shapkindev@gmail.com>
2019-01-19 12:52:38 +0300

    second commit

--- SwiftTableViewApp/SwiftTableViewApp/ViewController.swift
+++ SwiftTableViewApp/SwiftTableViewApp/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        let ac = UIAlertController(title: "Hello", message: "Hello world App", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        ac.addAction(alertAction)
+        present(ac, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
-
